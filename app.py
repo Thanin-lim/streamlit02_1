82c8eb10fef8bc17ea77ee59cfce789cb5c2a5f2
comseven <66056040@kmitl.ac.th>
2023-10-29 12:38:16 +0700

    add component show_pages

--- app.py
+++ app.py
@@ -2,15 +2,12 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
 from st_pages import Page, show_pages, add_page_title
 
 show_pages([
-    Page("app.py", "Home"),
-    Page("pages/tab.py", "Tab Layout"),
-    Page("pages/map.py", "Map Layout"),
-    #s
+    Page("app.py", "Home", "🏠"),
+    Page("pages/tab.py", "Tab Layout", "📖"),
+    Page("pages/map.py", "Map Layout", "🗺️"),
 ])
 # --- 01
 # https://docs.streamlit.io/library/api-reference/write-magic
